54ac3ac6b4a1973bea92ed3037c31b0bdf9a352e
rmay <159880101+rhiannawilson@users.noreply.github.com>
2024-05-15 21:48:27 +1000

    revisited javascript and HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,8 @@
 
 //  a confirmation dialog box top left of page, a message welcoming the user to the task board, and providing a brief run through on how to use the page.
 // then calling the confirm() function, OK and Cancel buttons. The function doesn't return any value. 
-function welcomeDialog () {
-    confirm(`Welcome to your task board! A place to manage all your tasks in one place. Happy Tasking! 
+function welcomeDialog() {
+  confirm(`Welcome to your task board! A place to manage all your tasks in one place. Happy Tasking! 
 
   1. Select the green ADD TASK button to create a new task
   2. Enter the details of your task
@@ -20,379 +20,173 @@ function welcomeDialog () {
 
   Additional:  
   - yellow tasks = near deadline
-  - red tasks = task overdue`);
+  - red tasks = task overdue`)
 };
-
-//  const modal = document.getElementById('myModal');
- const addTaskBtn = document.getElementById('addTaskBtn');
- const title = $("#task-title");
- const description = $("#task-description");
- const dueDate = $("#task-due-date");
- const toDoCards = $("#to-do-cards");
-// const renderTaskList = ('renderTaskList');
 
 
 // Retrieving data from localStorage, parsing it into JavaScript objects, and providing default values if the retrieved data is falsy or not present.
-let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-
-// Setting default value if nextId is falsy or not present in localStorage
-if (!nextId) {
-  nextId = 100; // Set the initial value to 100
-  localStorage.setItem('nextId', JSON.stringify(nextId)); // Store the initial value in localStorage
-};
 
 // A function to generate a unique task ID for each new task added to the task management system
 function generateTaskId() {
-  let currentNextId = nextId;
+ // if nextId does not exist in localStorage, set it to 1
+ if (nextId === null) {
+  nextId = 1;
+  // otherwise, increment it by 1
+} else {
   nextId++;
-  localStorage.setItem('nextId', JSON.stringify(nextId)); // Update the nextId in localStorage
-  return currentNextId;
+}
+// save nextId to localStorage
+localStorage.setItem('nextId', JSON.stringify(nextId));
+return nextId;
 };
-
-const taskId1 = generateTaskId();
-const taskId2 = generateTaskId();
-
-console.log("Generated Task ID 1:", taskId1);
-console.log("Generated Task ID 2:", taskId2);
-
 
 
 // generating a HTML markup for a task card, based on the information provided in the task object and make the task cards draggable
 function createTaskCard(task) {
-  let taskCard = `
-     <div class="taskCard" draggable="true">
-          <h3>${task.title}</h3>
-          <p>${task.description}</p>
-          <p>Due Date: ${task.dueDate}</p>
-          <p>Status: ${task.status}</p>
-     </div>
-   `;
+  // create card elements
+  const taskCard = $('<div>')
+    .addClass('card w-75 task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.description);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
 
-   return taskCard; // Return the generated HTML markup
+  // set card background color based on due date
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  // append card elements
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  return taskCard;
+
 };
 
 
-// Function to create and append task cards
-// function createTaskCard(task) {
-//   const taskListContainer = document.getElementById("toDoCards");
-
-//   const card = document.createElement("div");
-//   card.classList.add("card", "mb-3");
-
-//  const cardBody = document.createElement("div");
-//   cardBody.classList.add("card-body");
-
-// const title = document.createElement("h5");
-//  title.classList.add("card-title");
-//  title.textContent = task.title;
-
-//  const description = document.createElement("p");
-//   description.classList.add("card-text");
-//   description.textContent = task.description;
-
-//   const dueDate = document.createElement("p");
-//  dueDate.classList.add("card-text");
-//  dueDate.textContent = "" + task.dueDate;
-
-//  cardBody.appendChild(title);
-//   cardBody.appendChild(description);
-//  cardBody.appendChild(dueDate);
-
-//   card.appendChild(cardBody);
-// taskContainer.appendChild(card);
-
-// return taskCard;
-// };
-
-
-//////////////////////////// TEST CODE  ////////////////////////////
-// The following sample task object, helped me to test the above createTaskCard(task) function to see if it was working. 
-
-// // Define a sample task object
-// const sampleTask = {
-//   title: "Sample Task",
-//   description: "This is a sample task description.",
-//   dueDate: "2024-05-10",
-//   status: "Incomplete"
-// };
-
-// // Call the createTaskCard function with the sample task object
-// const taskCardMarkup = createTaskCard(sampleTask);
-
-// // Log the generated HTML markup to the console
-// console.log(taskCardMarkup);
-//////////////////////////// TEST CODE  ////////////////////////////
-
-
-
-
-
-
 // displays the list of tasks visually on the web page by generating HTML elements dynamically based on the data provided in the taskList array.
-function renderTaskList(taskList) {
-   const taskListContainer = $(".task-list-container");
-  taskListContainer.innerHTML = ""; // Clear previous content
+function renderTaskList() {
+  if (!tasks) {
+    tasks = []
+  }
+  //empty existing task cards
+  const toDoList = $('#todo-cards')
+  toDoList.empty();
+  const inProgressList = $('#in-progress-cards')
+  inProgressList.empty();
+  const doneList = $('#done-cards')
+  doneList.empty();
+  // looping through the tasks and creating task cards for each of the statuses - targets the status
+  for (let task of tasks) {
+    if (task.status === 'to-do') {
+      toDoList.append(createTaskCard(task))
+    } else if (task.status === 'in-progress') {
+      inProgressList.append(createTaskCard(task))
+    } else if (task.status === 'done') {
+      doneList.append(createTaskCard(task))
+    }
 
-   taskList.forEach(task => {
-      const taskCardHtml = createTaskCard(task);
-      $(`#${task.status}-cards`).append(createTaskCard(task));
-      applyStyle(task.id);
+  }
+    // make task cards draggable
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // function to clone the card being dragged so that the original card remains in place
+    helper: function (e) {
+      // check of the target of the drag event is the card itself or a child element if it is the card itself, clone it, otherwise find the parent card and clone that
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      return original.clone().css({
+        maxWidth: original.outerWidth(),
+      });
+    },
   });
- };
+};
 
-
-//   // Make the task card droppable
-//   $(taskCard).droppable({
-//       accept: '.draggable-task',
-//       drop: function(event, ui) {
-//           // Handle the drop event here
-//           const droppedTask = ui.draggable.data('task');
-//           // For example, you could move the dropped task to a new category
-//           droppedTask.status = 'In Progress';
-
-//                // Update the UI to reflect the change
-//             // For example, move the task card to the 'In Progress' section
-//             console.log(ui.draggable);
-
-//             $(ui.draggable).appendTo($(this));
-
-//       }
-//   });
-
-//   return taskCard;
-// };
-
-
-const tasks = []; // Define the task list globally
-
-function addTask(title, description, dueDate) {
+// this function handles a new task comes from jquery ready below - this function was originally called addTask!! 
+function submitTask(event) {
+  event.preventDefault()
+  //create new task
   let newTask = {
     id: generateTaskId(),
-    title: title,
-    description: description,
+    title: $('#taskTitle').val(),
+    description: $('#taskDescription').val(),
+    dueDate: $('#taskDueDate').val(),
+    status: 'to-do',
   };
 
+  // Add the new task to the tasks array - this is a method()
+  tasks.push(newTask);
+  // send it to localstorage 
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+  renderTaskList()
+  $('#taskTitle').val('');
+  $('#taskDescription').val('');
+  $('#taskDueDate').val('');
+};
 
-  // Add the new task to the tasks array
- tasks.push(newTask);
-   // Log the new task for verification
-    console.log(tasks);
- };
+// function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+  // get the task id and new status from the event
+  const taskId = ui.draggable[0].dataset.taskId;
+  const newStatus = event.target.id;
 
-
-
- 
-
-// // TO DO
-// function handleDeleteTask(event) {
-//   // Function logic goes here
-// }
-
-
-// Todo: create a function to handle dropping a task into a new status lane
- function handleDrop(event, ui) {
-
- };
-
-
-// // Event listeners
-document.addEventListener('DOMContentLoaded', function() {
-//   // Add event listeners after the DOM has loaded
-//   // Example: document.getElementById('deleteButton').addEventListener('click', handleDeleteTask);
- });
-
-
-
-const modal = document.getElementById('myModal');
-
-// Get the closeBtn element that closes the modal
-let closeBtn = document.querySelector('.close');
-let myModal = document.getElementById('myModal');
-
-// When the user clicks on the button, open the modal
-
-addTaskBtn.onclick = function() {
-  modal.style.display = 'block';
-}
-
-// Function to close the modal
-function closeModal() {
-    modal.style.display = 'none';
-}
-    // Event listener for the close button
-    closeBtn.addEventListener('click', closeModal);
-
-
-// Optional: Close the modal when clicking outside of it
-window.addEventListener('click', function(event) {
-    if (event.target === myModal) {
-        closeModal();
+  for (let task of tasks) {
+    // update the task status of the dragged card
+    if (task.id === parseInt(taskId)) {
+      task.status = newStatus;
     }
+  }
+  // save and render
+  localStorage.setItem('tasks', JSON.stringify(task));
+  renderTaskList();
+};
+
+
+
+// re-write this 
+function handleDeleteTask(event) {
+
+};
+
+// 135 - 155 complete !!
+// Event listeners - jquery syntax
+$(document).ready(function () {
+  // render the task list
+  renderTaskList();
+
+  // Event Listener 
+  $('#taskForm').on('submit', submitTask)
+
+  //make lanes droppable 
+  $('.lane').droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
+
+  //make due date field a date picker 
+  $('#taskDueDate').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
 });
-
-  // Make lanes droppable
-
-  
-
-  // // Make the due date field a date picker
-  // $(function () {
-  //     $('.datepicker').datepicker({
-  //         changeMonth: true,
-  //         changeYear: true,
-  //     });
-  // });
-
-// // Call the function to initialize the task board
- $(document).ready(function () {
- });
 
 
 // ** End of JavaScript File ** //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
